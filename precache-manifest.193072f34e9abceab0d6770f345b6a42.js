1dc58f86e5b064bede60f12bb06cc1adddf743b9
Kuchta Rafał <RKuchta@multikino.grupa.iti>
2019-03-25 20:52:05 +0100

    Updates

---
+++ precache-manifest.193072f34e9abceab0d6770f345b6a42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/app/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "10256b2700850e285109",
+    "url": "/app/static/js/runtime~main.10256b27.js"
+  },
+  {
+    "revision": "f1c8860e5107a7d3a736",
+    "url": "/app/static/js/main.f1c8860e.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/app/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "f1c8860e5107a7d3a736",
+    "url": "/app/static/css/main.8c8d9dde.chunk.css"
+  },
+  {
+    "revision": "882d0d3aaadbb924a26a2b526b893344",
+    "url": "/app/index.html"
+  }
+];
